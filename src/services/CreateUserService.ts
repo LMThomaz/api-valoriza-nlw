a348a87700e84e6f4c8029b8790077796391cf60
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-09-09 17:55:30 -0300

    [Mission: Node.js] Aula 02 – Maximum Speed

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,30 @@
+import { getCustomRepository } from 'typeorm';
+import { UsersRepositories } from '../repositories/UserRepositories';
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+export class CreateUserService {
+  async execute({ name, email, admin }: IUserRequest) {
+    const usersRepository = getCustomRepository(UsersRepositories);
+
+    if (!email) throw new Error('Email incorrect');
+
+    const userAlreadyExists = await usersRepository.findOne({ email });
+
+    if (userAlreadyExists) throw new Error('User already exists');
+
+    const user = usersRepository.create({
+      name,
+      email,
+      admin,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
